e32e28ffa62396d69f27dae7a696f374f26638d1
Erick Lumunge <lumungep12@gmail.com>
2021-10-14 03:12:45 +0300

    responsive design

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,21 +1,28 @@
-import { Typography } from '@mui/material';
-import landingImg from '../../img/home.svg';
+import { Typography } from "@mui/material";
+import landingImg from "../../img/home.svg";
 //styles
-import './styles.css';
+import "./styles.css";
 
 const Landing = () => {
-    // const classes = useStyles();
-
-    return(
-        <div className="landingContainer">
-           <div>
-                <Typography variant="h2">Lorem ipsum dolor sit amet consectetur adipisicing.</Typography>
-           </div>
-           <div>
-                <img src={landingImg} alt="landing_img" />
-           </div>
-        </div>
-    )
-}
+	// const classes = useStyles();
+ 
+	return (
+		<div className="landingContainer">
+			<div className="introText">
+				<p className="title">
+					Lorem ipsum dolor sit amet consectetur adipisicing.
+					<br />
+					<span className="subtitle">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, amet!
+					</span>
+				</p>
+            <button className="cta">Let's Go</button>
+			</div>
+			<div className="img">
+				<img src={landingImg} alt="landing_img" />
+			</div>
+		</div>
+	);
+};
 
 export default Landing;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,44 +1,67 @@
-import { Button } from "@mui/material";
 // icons
-import LanguageIcon from '@mui/icons-material/Language';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LanguageIcon from "@mui/icons-material/Language";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ExploreIcon from "@mui/icons-material/Explore";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 //styles
-import './styles.css';
-// import useStyles from './styles';
+import "./styles.css";
 
 const Navbar = () => {
-    // const classes = useStyles();
-    return(
-        <header className="navContainer">
-            <div>
-                logo here
-            </div>
-            <div className="navLinks">
-                <a href="#!" className="link">Places to Stay</a>
-                <a href="#!" className="link">Link 2</a>
-                <a href="#!" className="link">Link 3</a>
-            </div>
-            <div className="navEnd">
-                <div className="end">
-                    <button className="btn">Become a host</button>
-                </div>
-                <div className="end">
-                    <LanguageIcon/>
-                </div>
-                <div className="dropdown end">
-                    <AccountCircleIcon onClick={() => alert("hello")}/>
-                    <div className="dropContent">
-                        <a href="#!">Sign up</a>
-                        <a href="#!">Log in</a>
-                        <hr />
-                        <a href="#!">Share your home</a>
-                        <a href="#!">Share your home</a>
-                        <a href="#!">Help center</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
-}
+	return (
+		<header className="navContainer">
+			<div className="largeNav">
+				<div>logo here</div>
+				<div className="navLinks">
+					<a href="#!" className="link">
+						Places to Stay
+					</a>
+					<a href="#!" className="link">
+						Link 2
+					</a>
+					<a href="#!" className="link">
+						Link 3
+					</a>
+				</div>
+				<div className="navEnd">
+					<div className="end">
+						<button className="btn">Become a host</button>
+					</div>
+					<div className="end">
+						<LanguageIcon />
+					</div>
+					<div className="dropdown end">
+						<AccountCircleIcon onClick={() => alert("hello")} />
+						<div className="dropContent">
+							<a href="#!">Sign up</a>
+							<a href="#!">Log in</a>
+							<hr />
+							<a href="#!">Share your home</a>
+							<a href="#!">Share your home</a>
+							<a href="#!">Help center</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="smallNav">
+				<div className="smallIcon">
+					<ExploreIcon />
+                    <br />
+                    <small>spaces</small>
+				</div>
+				<div className="smallIcon">
+					<FavoriteBorderIcon />
+                    <br />
+                    <small>wishlist</small>
+				</div>
+				<div className="smallIcon">
+					<PersonOutlineIcon />
+                    <br />
+                    <small>login</small>
+				</div>
+			</div>
+		</header>
+	);
+};
 
 export default Navbar;
