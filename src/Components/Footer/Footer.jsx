63167b4ee6c8d7341845e6cf9d95c1be92aff2c9
Erick Lumunge <lumungep12@gmail.com>
2021-10-16 20:49:54 +0300

    responsive footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,34 +1,47 @@
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import './footer.css';
+import "./footer.css";
 
 const Footer = () => {
 	return (
 		<footer>
 			<div className="header">
-				<div className="footLinks">
+				<div className="footLinksContainer">
 					<p className="footHeading">About</p>
-					<a href="#!">How hom-ly works</a>
-					<a href="#!">Blog</a>
-					<a href="#!">Privacy policy</a>
-					<a href="#!">Terms and conditions</a>
+					<div className="footLinks">
+						<a href="#!">How hom-ly works</a>
+						<a href="#!">Blog</a>
+						<a href="#!">Privacy policy</a>
+						<a href="#!">Terms and conditions</a>
+					</div>
 				</div>
-				<div className="footLinks">
+				<div className="footLinksContainer">
 					<p className="footHeading">Community</p>
-					<a href="#!">Coupons</a>
-					<a href="#!">Referrals</a>
-					<a href="#!">Accessibility</a>
-					<a href="#!">Diversity and belonging</a>
+					<div className="footLinks">
+						<a href="#!">Coupons</a>
+						<a href="#!">Referrals</a>
+						<a href="#!">Accessibility</a>
+						<a href="#!">Diversity and belonging</a>
+					</div>
 				</div>
-				<div className="footLinks">
+				<div className="footLinksContainer">
 					<p className="footHeading">Host</p>
-					<a href="#!">Share your space</a>
+					<div className="footLinks">
+						<a href="#!">Host your house</a>
+						<a href="#!">Find Host</a>
+						<a href="#!">Find Host</a>
+						<a href="#!">Find Host</a>
+					</div>
 				</div>
-				<div className="footLinks">
+				<div className="footLinksContainer">
 					<p className="footHeading">Support</p>
-					<a href="#!">Trust and safety</a>
-					<a href="#!">Help center</a>
+					<div className="footLinks">
+						<a href="#!">Trust and safety</a>
+						<a href="#!">Help center</a>
+						<a href="#!">Trust and safety</a>
+						<a href="#!">Trust and safety</a>
+					</div>
 				</div>
 			</div>
 			<div className="footer">
