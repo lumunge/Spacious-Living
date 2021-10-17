09952a7b1dcc92dff9135650ac4d98bd54d6721b
Erick Lumunge <lumungep12@gmail.com>
2021-10-17 04:26:08 +0300

    responsive landing new house

--- src/Components/UploadComponents/Landing/Landing.jsx
+++ src/Components/UploadComponents/Landing/Landing.jsx
@@ -1,17 +1,18 @@
-import './landing.css';
-
+import "./landing.css";
 
 const Landing = () => {
 	return (
 		<div className="landingWrapper">
 			<div className="content">
-				<p className="landingHeading">Host your house</p>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-					Consequatur labore nostrum libero ipsa sint molestiae
-					adipisci fugit ducimus tenetur recusandae!
-				</p>
-                <button className="cta uploadCta">Let's go</button>
+				<div className="contentText">
+					<p className="landingHeading">Host your house</p>
+					<p className="landingText">
+						Lorem, ipsum dolor sit amet consectetur adipisicing
+						elit. Consequatur labore nostrum libero ipsa sint
+						molestiae adipisci fugit ducimus tenetur recusandae!
+					</p>
+					<button className="cta uploadCta">Let's go</button>
+				</div>
 			</div>
 		</div>
 	);
